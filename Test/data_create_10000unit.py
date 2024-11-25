cbf84a247324ac1bbba1b1bc18b3449cfc6f594d
tcpu1005 <jihun1005@gmail.com>
2024-11-25 09:40:47 +0900

    종합설계프로젝트 최종 완성

---
+++ Test/data_create_10000unit.py
@@ -0,0 +1,54 @@
+# data_create_10000unit.py
+
+import requests
+import time
+import pandas as pd
+import ast
+import re
+import random
+
+def load_data_from_csv():
+    # CSV 파일 읽기
+    df_traces = pd.read_csv('Event_traces_10000.csv')
+    df_labels = pd.read_csv('anomaly_label_10000.csv')
+
+    # BlockId를 기준으로 데이터 병합 (접미사 지정)
+    df = pd.merge(df_traces, df_labels, on='BlockId', suffixes=('_traces', '_labels'))
+
+    # 병합된 데이터프레임의 컬럼명 확인
+    print("Merged df columns:", df.columns.tolist())
+
+    data_list = []
+    for index, row in df.iterrows():
+        block_id = row['BlockId']
+        features = row['Features']
+        label = row['Label_labels']  # df_labels의 'Label' 컬럼 사용
+
+        # 문자열 형태의 리스트를 올바른 형식으로 변환
+        features_converted = re.sub(r'(\bE\d+\b)', r'"\1"', features)
+
+        # 문자열 형태의 리스트를 실제 리스트로 변환
+        events = ast.literal_eval(features_converted)
+
+        data_list.append({
+            'block_id': block_id,
+            'events': events,
+            'label': label
+        })
+
+    return data_list
+
+def simulate_data_stream():
+    # CSV 파일에서 데이터를 로드합니다.
+    data_list = load_data_from_csv()
+
+    # 데이터 순서를 섞어서 시뮬레이션의 현실감을 높입니다.
+    random.shuffle(data_list)
+
+    for data in data_list:
+        response = requests.post('http://localhost:5000/predict', json=data)
+        print(response.json())
+        time.sleep(2)  # 2초 간격으로 데이터 전송
+
+if __name__ == '__main__':
+    simulate_data_stream()
--- backend/models/gnn_model.py
+++ backend/models/gnn_model.py
@@ -1,20 +1,20 @@
-# backend/models/gnn_model.py
 import torch
 import torch.nn.functional as F
+from torch.nn import Linear
+import torch.nn as nn
 from torch_geometric.nn import GCNConv, global_mean_pool
-from torch.nn import Linear, Dropout
 
 class GNN(torch.nn.Module):
     def __init__(self, num_node_features, hidden_channels, dropout=0.6):
         super(GNN, self).__init__()
         self.conv1 = GCNConv(num_node_features, hidden_channels)
-        self.dropout1 = Dropout(dropout)
+        self.dropout1 = nn.Dropout(dropout)
         self.conv2 = GCNConv(hidden_channels, hidden_channels)
-        self.dropout2 = Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
         self.lin = Linear(hidden_channels, 2)  # 그래프 분류를 위한 출력 레이어
-        self.dropout3 = Dropout(dropout)
+        self.dropout3 = nn.Dropout(dropout)
 
-    def forward(self, x, edge_index, batch):
+    def forward(self, x, edge_index, batch): #이게 진짜임..우리는 이걸 쓰는거야. init는 디폴트 호출 때 사용되는 거다잉.
         x = self.conv1(x, edge_index)
         x = F.relu(x)
         x = self.dropout1(x)
